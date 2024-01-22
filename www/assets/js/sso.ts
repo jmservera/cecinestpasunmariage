d0d6bb5d799899f91f42cc41e5825d70f1ef8e14
jmservera <8036360+jmservera@users.noreply.github.com>
2024-01-22 09:12:00 +0100

    temporary fix for .referrer

--- www/assets/js/sso.ts
+++ www/assets/js/sso.ts
@@ -1,6 +1,10 @@
 (() => {
-  var links = document.querySelectorAll<HTMLLinkElement>("ul#sso li a");
-  links.forEach((link) => {
-    link.href = link.href + window.location.search;
-  });
+  if (window.location.search.endsWith(".referrer")) {
+    console.log("Referrer replace failed, redirecting to main page");
+  } else {
+    var links = document.querySelectorAll<HTMLLinkElement>("ul#sso li a");
+    links.forEach((link) => {
+      link.href = link.href + window.location.search;
+    });
+  }
 })();
