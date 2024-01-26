afc37e7b8b05691cab3f71816c1abb6214bdee70
jmservera <8036360+jmservera@users.noreply.github.com>
2024-01-26 23:22:09 +0100

    use local db for location

--- www/assets/js/sso.ts
+++ www/assets/js/sso.ts
@@ -1,10 +1,15 @@
-(() => {
-  if (window.location.search.endsWith(".referrer")) {
-    console.log("Referrer replace failed, redirecting to main page");
-  } else {
-    var links = document.querySelectorAll<HTMLLinkElement>("ul#sso li a");
-    links.forEach((link) => {
-      link.href = link.href + window.location.search;
-    });
+let search: string = window.location.search;
+if (search.endsWith(".referrer")) {
+  console.log("Referrer replace failed, redirecting to main page");
+  const url: string = localStorage.getItem("lastPage");
+  if (url) {
+    search = search.replace(".referrer", url);
   }
-})();
+  else {
+    search = search.replace(".referrer", "/");
+  }
+}
+var links = document.querySelectorAll<HTMLLinkElement>("ul#sso li a");
+links.forEach((link) => {
+  link.href = link.href + window.location.search;
+});
