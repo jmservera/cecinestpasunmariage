68328cb5d7a8b75f4508c9c8e5affeb66c184f56
jmservera <8036360+jmservera@users.noreply.github.com>
2024-01-29 17:42:02 +0000

    add timeout

--- www/assets/js/loading.ts
+++ www/assets/js/loading.ts
@@ -1,7 +1,12 @@
 const loading = document.querySelector("#loading-panel");
 
-export function showLoading() {
+export function showLoading(timeout: number = 0) {
   loading.classList.remove("hidden");
+  if (timeout > 0) {
+    setTimeout(() => {
+      hideLoading();
+    }, timeout);
+  }
 }
 
 export function hideLoading() {
