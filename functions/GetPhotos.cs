477ef497b51f2756b5d4362fa8504fada1c85dc6
jmservera <8036360+jmservera@users.noreply.github.com>
2024-06-22 22:28:52 +0000

    refactor: Improve photo retrieval and storage in GetPhotos.cs
    This commit refactors the GetPhotos.cs file to improve the retrieval and storage of photos. It introduces a new logic to find the original picture for each thumbnail and adds it to the response. This enhancement allows for a more efficient and accurate display of photos in the application.

--- functions/GetPhotos.cs
+++ functions/GetPhotos.cs
@@ -37,7 +37,7 @@ namespace functions
 
             var allTumbThePics = containerThumbnailsClient.GetBlobs().ToList().OrderBy(b => b.Name);
             photosResponse.NumPictures = allTumbThePics.Count();
-
+            var allPics = containerPicsClient.GetBlobs();
 
             // todo: use https://learn.microsoft.com/en-us/azure/architecture/web-apps/guides/security/secure-single-page-application-authorization
             // to secure the access to the blob storage
@@ -47,17 +47,24 @@ namespace functions
                 BlobClient blobThumbClient = containerThumbnailsClient.GetBlobClient(blobTumbItem.Name);
                 var blobSasUriThumb = blobThumbClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddMinutes(15));
 
-                BlobClient blobPicsClient = containerPicsClient.GetBlobClient(blobTumbItem.Name);
-                var blobSasUriPics = blobPicsClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddMinutes(15));
-
-
-                photosResponse.Pictures.Add(new Photo()
+                // extensions may differ between the thumbnail and the picture
+                var noExtension = Path.Join(Path.GetDirectoryName(blobTumbItem.Name), Path.GetFileNameWithoutExtension(blobTumbItem.Name));
+                // find the original picture for the thumbnail
+                var pic = allPics.Where(b => Path.Join(Path.GetDirectoryName(b.Name), Path.GetFileNameWithoutExtension(b.Name)) == noExtension).FirstOrDefault();
+                if (pic is { })
                 {
-                    Name = blobPicsClient.Name.ToString(),
-                    Uri = blobSasUriPics.ToString(),
-                    ThumbnailUri = blobSasUriThumb.ToString()
-                });
+                    BlobClient blobPicsClient = containerPicsClient.GetBlobClient(pic.Name);
+                    var blobSasUriPics = blobPicsClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddMinutes(15));
+
+                    photosResponse.Pictures.Add(new Photo()
+                    {
+                        Name = blobPicsClient.Name.ToString(),
+                        Uri = blobSasUriPics.ToString(),
+                        ThumbnailUri = blobSasUriThumb.ToString()
+                    });
+                }
             }
+
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "application/json; charset=utf-8");
