a6d564c38bfd5bc6b484d6fb2769c61d4a79c45b
jmservera <8036360+jmservera@users.noreply.github.com>
2024-07-01 09:41:38 +0000

    add data preparation and check

---
+++ backend/infra/DataCheck/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using dotenv.net;
+using functions.AI;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Text.RegularExpressions;
+
+static async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient faceClient, Stream stream, string recognition_model)
+{
+    // Detect faces from image URL. Since only recognizing, use the recognition model 1.
+    // We use detection model 3 because we are not retrieving attributes.
+    IList<DetectedFace> detectedFaces = await faceClient.Face.DetectWithStreamAsync(stream, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection03, returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.QualityForRecognition });
+    List<DetectedFace> sufficientQualityFaces = new List<DetectedFace>();
+    foreach (DetectedFace detectedFace in detectedFaces)
+    {
+        var faceQualityForRecognition = detectedFace.FaceAttributes.QualityForRecognition;
+        if (faceQualityForRecognition.HasValue && (faceQualityForRecognition.Value >= QualityForRecognition.Medium))
+        {
+            sufficientQualityFaces.Add(detectedFace);
+        }
+    }
+    Console.WriteLine($"{detectedFaces.Count} face(s) with {sufficientQualityFaces.Count} having sufficient quality for recognition detected from image");
+
+    return sufficientQualityFaces;
+}
+
+DotEnv.Load(options: new DotEnvOptions(envFilePaths: ["../../../www/.env"], ignoreExceptions: false));
+
+var key = Environment.GetEnvironmentVariable("VISION_KEY");
+var endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
+var client = new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+
+var baseDirectory = "/cecidata/";
+
+List<string> images = ["BetJuanma_Crowd.jpg","IsaJuanma.jpg","JuanmaOvi.jpg",
+"LucBetCrowd.jpg","LucBet.jpg","LucBet2.jpg","LucBet3.jpg","group_with_Luc.jpg","IsaPerla.jpg","LucBetEdurne.jpg"];
+
+var personGroupId = FaceRecognition.DefaultPersonGroupId;
+var recognitionModel = Microsoft.Azure.CognitiveServices.Vision.Face.Models.RecognitionModel.Recognition04;
+
+foreach (var sourceImageFileName in images)
+{
+    Console.WriteLine($"Check whether image is of sufficient quality for recognition");
+    using var stream = new FileStream(Path.Combine(baseDirectory, sourceImageFileName), FileMode.Open);
+
+    List<Guid> sourceFaceIds = new List<Guid>();
+    List<DetectedFace> detectedFaces = await DetectFaceRecognize(client, stream, recognitionModel);
+
+    // Add detected faceId to sourceFaceIds.
+    foreach (var detectedFace in detectedFaces) { sourceFaceIds.Add(detectedFace.FaceId.Value); }
+
+    // Identify the faces in a person group. 
+    var identifyResults = await client.Face.IdentifyAsync(sourceFaceIds, personGroupId);
+
+    foreach (var identifyResult in identifyResults)
+    {
+        if (identifyResult.Candidates.Count == 0)
+        {
+            Console.WriteLine($"No person is identified for the face in: {sourceImageFileName} - {identifyResult.FaceId},");
+            continue;
+        }
+        Person person = await client.PersonGroupPerson.GetAsync(personGroupId, identifyResult.Candidates[0].PersonId);
+        Console.WriteLine($"Person '{person.Name}' is identified for the face in: {sourceImageFileName} - {identifyResult.FaceId}," +
+            $" confidence: {identifyResult.Candidates[0].Confidence}.");
+
+        VerifyResult verifyResult = await client.Face.VerifyFaceToPersonAsync(identifyResult.FaceId, person.PersonId, personGroupId);
+        Console.WriteLine($"Verification result: is a match? {verifyResult.IsIdentical}. confidence: {verifyResult.Confidence}");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
---
+++ backend/infra/DataPreparation/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using dotenv.net;
+using functions.AI;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Text.RegularExpressions;
+
+DotEnv.Load(options: new DotEnvOptions(envFilePaths: ["../../../www/.env"], ignoreExceptions: false));
+
+var key = Environment.GetEnvironmentVariable("VISION_KEY");
+var endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
+var client = new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+
+var baseDirectory = "/cecidata/";
+// Create a dictionary for all your images, grouping similar ones under the same key.
+var personDictionary = new Dictionary<string, string[]>
+    { { "Isa", [ "isa_1.jpg", "isa_2.jpg", "isa_3.jpg", "isa_4.jpg" ] },
+            { "Juanma",  ["juanma_1.jpg","juanma_2.jpg" ] },
+            { "Luc", ["Luc_1.jpg","Luc_2.jpg","Luc_3.jpg"] },
+            { "Bet", ["bet_1.jpg", "bet_2.jpg", "bet_3.jpg", "bet_4.jpg", "bet_5.png" ]}
+            //,{ "Perla",  ["perla_1.jpg", "perla_2.jpg" ] }
+    };
+
+var personGroupId = FaceRecognition.DefaultPersonGroupId;
+var recognitionModel = Microsoft.Azure.CognitiveServices.Vision.Face.Models.RecognitionModel.Recognition04;
+
+var group = await client.PersonGroup.GetAsync(personGroupId);
+if (group!=null)
+{
+    Console.WriteLine($"Delete person group {personGroupId}.");
+    await client.PersonGroup.DeleteAsync(personGroupId);
+}
+
+Console.WriteLine($"Create a person group ({personGroupId}).");
+await client.PersonGroup.CreateAsync(personGroupId, personGroupId, recognitionModel: recognitionModel);
+
+// The similar faces will be grouped into a single person group person.
+foreach (var groupedFace in personDictionary.Keys)
+{
+    // Limit TPS
+    await Task.Delay(250);
+    Person person = await client.PersonGroupPerson.CreateAsync(personGroupId: personGroupId, name: groupedFace);
+    Console.WriteLine($"Create a person group person '{groupedFace}'.");
+
+    // Add face to the person group person.
+    foreach (var similarImage in personDictionary[groupedFace])
+    {
+        Console.WriteLine($"Check whether image is of sufficient quality for recognition");
+        using var stream = new FileStream(Path.Combine(baseDirectory, similarImage), FileMode.Open);
+        IList<DetectedFace> detectedFaces1 = await client.Face.DetectWithStreamAsync(stream,
+            recognitionModel: recognitionModel,
+            detectionModel: DetectionModel.Detection03,
+            returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.QualityForRecognition });
+        bool sufficientQuality = true;
+        foreach (var face1 in detectedFaces1)
+        {
+            var faceQualityForRecognition = face1.FaceAttributes.QualityForRecognition;
+            //  Only "high" quality images are recommended for person enrollment
+            if (faceQualityForRecognition.HasValue && (faceQualityForRecognition.Value != QualityForRecognition.High))
+            {
+                sufficientQuality = false;
+                break;
+            }
+        }
+
+        if (!sufficientQuality)
+        {
+            continue;
+        }
+
+        // add face to the person group
+        Console.WriteLine($"Add face to the person group person({groupedFace}) from image `{similarImage}`");
+        
+        using var faceStream = new FileStream(Path.Combine(baseDirectory, similarImage), FileMode.Open);
+        PersistedFace face = await client.PersonGroupPerson.AddFaceFromStreamAsync(personGroupId, person.PersonId,
+            faceStream, similarImage);
+    }
+}
+
+// Start to train the person group.
+Console.WriteLine();
+Console.WriteLine($"Train person group {personGroupId}.");
+await client.PersonGroup.TrainAsync(personGroupId);
+
+// Wait until the training is completed.
+while (true)
+{
+    await Task.Delay(1000);
+    var trainingStatus = await client.PersonGroup.GetTrainingStatusAsync(personGroupId);
+    Console.WriteLine($"Training status: {trainingStatus.Status}.");
+    if (trainingStatus.Status == TrainingStatusType.Succeeded) { break; }
+}
+Console.WriteLine();
