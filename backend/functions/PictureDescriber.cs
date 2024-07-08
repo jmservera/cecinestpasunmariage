7934cac766012c2c41ce726d1e946d7dfc00b5d0
jmservera <8036360+jmservera@users.noreply.github.com>
2024-07-08 22:16:30 +0000

    feat: remove repeated code

--- backend/functions/PictureDescriber.cs
+++ backend/functions/PictureDescriber.cs
@@ -1,32 +1,26 @@
-using System.IO;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization.Metadata;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Azure;
 using Azure.AI.Vision.ImageAnalysis;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using functions.AI;
-using functions.Storage;
 using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using SixLabors.ImageSharp;
-using Telegram.Bot.Types;
+
+using functions.AI;
+using functions.Storage;
 
 namespace functions
 {
     public partial class PictureDescriber(ILogger<PictureDescriber> logger, IFaceClient faceClient,
-     ImageAnalysisClient imageClient,
-     IStorageManager storageManager,
-    IChatCompletionService chatCompletionService,
-    IConfiguration configuration)
+                                            ImageAnalysisClient imageClient,
+                                            IStorageManager storageManager,
+                                            IChatCompletionService chatCompletionService,
+                                            IConfiguration configuration)
     {
         [GeneratedRegex("^\"|\"$")]
         private static partial Regex RemoveDoubleQuotes();
@@ -99,33 +93,7 @@ namespace functions
                 await thumbnail.ReadAsync(image);
                 try
                 {
-                    var history = new ChatHistory();
-
-                    history.AddSystemMessage("You are an AI assistant that helps people find a funny description or title of pictures that may contain people known by the requester, in English, French and Spanish, " +
-                    "considering the destination language characteristics" +
-                    (people.Count > 0 ? "and do not translate the names for the people in the picture. " : "") +
-                    "The output should be a json file with \"en\", \"fr\" and \"es\" as keys for the translations without any Markdown, just plain json. Here is the output schema:\n" +
-                    "{\n\"en\":\"English description\",\n\"fr\": \"French translation\"\n,\n\"es\": \"Spanish translation\"}\n");
-
-                    ImageContent imageContent = new(new ReadOnlyMemory<byte>(image), contentType);
-                    var items = new ChatMessageContentItemCollection{
-                        new TextContent(people.Count>0?$"In this picture you see the following people: {string.Join(',',people)}. Please find a funny title for this picture that includes the provided names.":
-                        "Please find a funny title for this picture."), imageContent};
-                    history.AddUserMessage(items);
-
-                    var result = await _chatCompletionService.GetChatMessageContentAsync(history);
-                    var description = result.Content ?? throw new InvalidOperationException("No translations found");
-                    var translationsd = JsonSerializer.Deserialize<Dictionary<string, string>>(description) ?? throw new InvalidOperationException("No translations found");
-                    if (translationsd != null)
-                    {
-                        return translationsd.Select(
-                            s => new KeyValuePair<string, string>(s.Key,
-                                //url encode string to be stored in metadata
-                                Uri.EscapeDataString(
-                                    //remove double quotes
-                                    RemoveDoubleQuotes().Replace(s.Value, "")
-                                ))).ToDictionary();
-                    }
+                    return await GenerateDescritptionCoreAsync(contentType, image, people);
                 }
                 catch (HttpOperationException ex)
                 {
@@ -139,36 +107,52 @@ namespace functions
                     var analysisResult = await _imageClient.AnalyzeAsync(BinaryData.FromBytes(image), VisualFeatures.DenseCaptions);
                     var caption = string.Join(", ", analysisResult.Value.DenseCaptions.Values.Select(v => v.Text).Distinct());
                     _logger.LogInformation("Generated description using Azure Cognitive Services for blob {name}: {caption}", name, caption);
-                    var dict = new Dictionary<string, string> { { "en", caption } };
-                    var history = new ChatHistory();
-                    history.AddSystemMessage("You are an AI assistant that based on a picture description you transform it to a funny sentence in English, French and Spanish.\n" +
-                    "The output should be a json file with \"en\", \"fr\" and \"es\" as keys for the translations without any Markdown, just plain json. Here is the output schema:\n" +
-                    "{\n\"en\":\"English description\",\n\"fr\": \"French translation\"\n,\n\"es\": \"Spanish translation\"}\n");
-                    history.AddUserMessage($"Here is the description for the picture: {caption}\n" +
-                    $"Here's the people that appear in the picture: {string.Join(',', people)}\n" +
-                    "Please provide a funny sentence for this description.");
-                    var descriptions = await _chatCompletionService.GetChatMessageContentAsync(history)?? throw new InvalidOperationException("No translations found");
-                    var translationsDict = JsonSerializer.Deserialize<Dictionary<string, string>>(descriptions.Content);
-                    if (translationsDict != null)
-                    {
-                        // add translationsDict to dict
-                        foreach (var (key, value) in translationsDict)
-                        {
-                            dict[key] = value;
-                        }
-                    }
-                    var finalResult = dict.Select(s => new KeyValuePair<string, string>(s.Key,
-                            //url encode string to be stored in metadata
-                            Uri.EscapeDataString(
-                                //remove double quotes
-                                RemoveDoubleQuotes().Replace(s.Value, "")
-                            ))).ToDictionary();
 
-                    return finalResult;
+                    return await GenerateDescritptionCoreAsync(contentType, null, people, caption);
 
                 }
             }
             return [];
+        }
+
+        private async Task<Dictionary<string, string>> GenerateDescritptionCoreAsync(string contentType, byte[]? image, IReadOnlyList<string> people, string? captions = null)
+        {
+#pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            OpenAIPromptExecutionSettings settings = new() { ResponseFormat = "json_object" };
+#pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            var history = new ChatHistory();
+
+            history.AddSystemMessage("You are an AI assistant that helps people find a funny description or title of pictures that may contain people known by the requester, in English, French and Spanish, " +
+            "considering the destination language characteristics" +
+            (people.Count > 0 ? " and do not translate the names for the people in the picture. " : "") +
+            "The output should be a json file with the schema:\n" +
+            "{\n\"en\":\"English description\",\n\"fr\": \"French translation\"\n,\n\"es\": \"Spanish translation\"}\n");
+
+            ChatMessageContentItemCollection items = [];
+            if (captions != null)
+            {
+                items.Add(new TextContent($"Here is the description for the picture: {captions}\n"));
+            }
+
+            items.Add(new TextContent(people.Count > 0 ? $"In this picture you see the following people: {string.Join(',', people)}. Please find a funny title for this picture that includes the provided names." :
+                        "Please find a funny title for this picture."));
+            if (image != null)
+            {
+                ImageContent imageContent = new(new ReadOnlyMemory<byte>(image), contentType);
+                items.Add(imageContent);
+            }
+            history.AddUserMessage(items);
+
+            var result = await _chatCompletionService.GetChatMessageContentAsync(history, settings);
+            var description = result.Content ?? throw new InvalidOperationException("No translations found");
+            var translationsd = JsonSerializer.Deserialize<Dictionary<string, string>>(description) ?? throw new InvalidOperationException("No translations found");
+            return translationsd.Select(
+                s => new KeyValuePair<string, string>(s.Key,
+                    //url encode string to be stored in metadata
+                    Uri.EscapeDataString(
+                        //remove double quotes
+                        RemoveDoubleQuotes().Replace(s.Value, "")
+                    ))).ToDictionary();
         }
     }
 }
