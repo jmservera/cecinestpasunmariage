c397459fc2e4ac943356e9b1daa6ebd2f1f803e9
jmservera <8036360+jmservera@users.noreply.github.com>
2024-10-07 05:25:56 +0000

    carrousel with the pictures Fixes #36

--- www/assets/js/gallery.ts
+++ www/assets/js/gallery.ts
@@ -1,34 +1,7 @@
 import { hideLoading, showLoading } from "./loading";
 import { getTranslation } from "./i18n";
 
-// import Swiper JS
-import Swiper from 'swiper';
-import {Manipulation, Navigation, Pagination, Autoplay} from 'swiper/modules';
-
-// use https://swiperjs.com/swiper-api
-const swiper = new Swiper('.swiper', {modules:[Manipulation, Navigation, Pagination, Autoplay], loop: false, autoplay: {delay: 5000},
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }});
-let lastIndex:number=-1;
-function playSlide(){
-  if(lastIndex!=-1){
-    const video = swiper.slides[lastIndex].querySelector('video');
-    if(video){
-      video.pause();
-    }
-    lastIndex=-1;
-  }
-  swiper.slides[swiper.activeIndex].querySelectorAll('video').forEach((video:HTMLVideoElement)=>{
-    video.play();
-    lastIndex=swiper.activeIndex;
-  });
-}
-swiper.on('activeIndexChange', playSlide);
-
 let current_page: number = 1; // the current page
-
 
 async function gallery(page: number = current_page): Promise<void> {
   showLoading();
@@ -92,36 +65,13 @@ async function gallery(page: number = current_page): Promise<void> {
       gallery(next_page);
     });
 
-    swiper.autoplay.stop();
-    swiper.removeAllSlides();
+    let i: number = 0;
+    $("#mygallery").html("");
     data.Pictures.forEach((element: any) => {
-      const slide = document.createElement('div') as HTMLDivElement;
-      slide.className = "swiper-slide imgbox";
-      const title = document.createElement('p') as HTMLParagraphElement;
-      title.textContent=element.Description;
-      slide.appendChild(title);
-      if(element.Uri.includes(".mp4")){
-        const item = document.createElement('video') as HTMLVideoElement;
-        item.className="center-fit";
-        item.autoplay=false;
-        item.src=element.Uri;
-        item.title=element.Description;
-        slide.appendChild(item);      }
-      else{
-        const item = document.createElement('img') as HTMLImageElement;
-        item.className="center-fit";
-        item.src=element.Uri;
-        item.title=element.Description;
-        item.alt=element.Description;
-        slide.appendChild(item);
-      }
-      swiper.appendSlide(slide);
+      i++;
+      //add elements to mygallery
+      $("#mygallery").append(`<div><a href="${element.Uri}" target="_blank"><img class="grid-item grid-item-${i}" src="${element.ThumbnailUri}" alt="${element.Description}" /></a><p>${element.Description}</p></div>`);
     });
-    swiper.update();
-    swiper.autoplay.start();
-    playSlide();
-
-
   } catch (error) {
     console.error("Error:", error);
     showStatus(error, "error");
