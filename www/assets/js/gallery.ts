95c8a576982b1a586c3968c71ab7dc0242ae11e7
jmservera <8036360+jmservera@users.noreply.github.com>
2024-12-08 23:26:01 +0000

    Refactor gallery pagination buttons and improve code readability

--- www/assets/js/gallery.ts
+++ www/assets/js/gallery.ts
@@ -11,19 +11,19 @@ async function gallery(page: number = current_page): Promise<void> {
 
     current_page = page; // the current page
     const response = await fetch(
-      `/api/GetPhotos?page=${current_page}&n=${pictures_per_page}&lang=${window.lang}`,
+      `/api/GetPhotos?page=${current_page}&n=${pictures_per_page}&lang=${window.lang}`
     ); // replace with your API endpoint
     const data = await response.json();
 
     const total_num_of_pictures: number = data.NumPictures; // total number of pictures
     $("#pages2").html("");
     $("#pages2").append(
-      `<div style="width: 100%;"> Número total de fotos: ${total_num_of_pictures} </div>`,
+      `<div style="width: 100%;"> Número total de fotos: ${total_num_of_pictures} </div>`
     );
 
     $("#pages").html("");
     const num_of_pages: number = Math.ceil(
-      total_num_of_pictures / pictures_per_page,
+      total_num_of_pictures / pictures_per_page
     );
 
     let inicial: number = 0;
@@ -31,16 +31,16 @@ async function gallery(page: number = current_page): Promise<void> {
 
     if (num_of_pages > max_num_of_pages) {
       if (current_page > 0) {
-        $("#pages").append(`<button class="page" id="-1" ><</button>`);
+        $("#pages").append(`<button class="pageNumber" id="-1" ><</button>`);
       }
 
-      if (current_page > Math.floor(max_num_of_pages / 2)) {
-        inicial = current_page - Math.floor(max_num_of_pages / 2);
-        if (current_page + Math.floor(max_num_of_pages / 2) <= num_of_pages) {
-          final = current_page + Math.floor(max_num_of_pages / 2);
-          inicial = final - max_num_of_pages + 1;
-        }
-      } else {
+      final = current_page + Math.ceil(max_num_of_pages / 2);
+      if (final > num_of_pages) {
+        final = num_of_pages;
+      }
+      inicial = final - max_num_of_pages;
+      if (inicial < 0) {
+        inicial = 0;
         final = max_num_of_pages;
       }
     }
@@ -48,34 +48,22 @@ async function gallery(page: number = current_page): Promise<void> {
     for (let i = inicial; i < final; i++) {
       if (i == current_page) {
         $("#pages").append(
-          `<button class="pageNumber selected" id="${i}" >${i + 1}</button>`,
+          `<button class="pageNumber selected" id="${i}" >${i + 1}</button>`
         );
       } else {
         $("#pages").append(
-          `<button class="pageNumber" id="${i}" >${i + 1}</button>`,
+          `<button class="pageNumber" id="${i}" >${i + 1}</button>`
         );
       }
     }
 
-    if (num_of_pages > max_num_of_pages && current_page < num_of_pages) {
-      $("#pages").append(`<button class="page" id="10000" >></button>`);
+    if (num_of_pages > max_num_of_pages && current_page < num_of_pages - 1) {
+      $("#pages").append(`<button class="pageNumber" id="10000" >></button>`);
     }
 
     // Add click event listeners to the page buttons
     $(".pageNumber").on("click", function () {
       $(".pageNumber").off("click"); // remove all the clicks before starting anew
-      let next_page: number = parseInt($(this).attr("id"));
-      if (next_page === -1) {
-        next_page = current_page - 1;
-      } else if (next_page == 10000) {
-        next_page = current_page + 1;
-      }
-      gallery(next_page);
-    });
-
-    // Add click event listeners to the arrow buttons
-    $(".page").on("click", function () {
-      $(".page").off("click"); // remove all the clicks before starting anew
       let next_page: number = parseInt($(this).attr("id"));
       if (next_page === -1) {
         next_page = current_page - 1;
@@ -91,7 +79,7 @@ async function gallery(page: number = current_page): Promise<void> {
       i++;
       //add elements to mygallery
       $("#mygallery").append(
-        `<div><a href="${element.Uri}" target="_blank"><img class="grid-item grid-item-${i}" src="${element.ThumbnailUri}" alt="${element.Description}" /></a><p>${element.Description}</p></div>`,
+        `<div><a href="${element.Uri}" target="_blank"><img class="grid-item grid-item-${i}" src="${element.ThumbnailUri}" alt="${element.Description}" /></a><p>${element.Description}</p></div>`
       );
     });
   } catch (error) {
